f2bd8f5401d4109dd1ad7eea46f0774788a14459
inferjay <inferjay@gmail.com>
2017-10-31 23:38:29 +0800

    Add email subscibe and modify css file author info and fixed span tag error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 /*
-* @Author: ZHC
+* @Author: inferjay
 * @Date:   2017-10-28 21:16:26
-* @Last Modified by:   ZHC
-* @Last Modified time: 2017-10-28 21:18:07
+* @Last Modified by:   inferjay
+* @Last Modified time: 2017-10-31 23:35:54
 */
 var $$ = mdui.JQ;
 
@@ -22,3 +22,64 @@ $('.follow-us-social-item.wechat').hover(function() {
 }, function() {
 		wechatQrcode.hide();
 });
+
+function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+
+// 邮件订阅功能
+var mailchimp_url = "https://androidlinks.us17.list-manage.com/subscribe/?u=21b7c731d4d2719058edaf040&id=6b25b5980e";
+var success_message = "请检查您收件箱并确认邮件";
+var msgParams = {'timeout': 2000, 'position': 'top'};
+
+var attemptSubscibe = function(e) {
+    e.preventDefault();
+    $('.btn-email-subscibe').attr('disabled', 'disabled');
+    var email = $('#email').val();
+    if (email != '' && IsEmail(email)) {
+    	msgParams['message'] = '请稍等...';
+        var snackbar = mdui.snackbar(msgParams);
+        mailchimp_url = mailchimp_url.replace('?u=', '/post-json?u=').concat('&c=?');
+        var data = {};
+        data['EMAIL'] = email;
+        $.ajax({
+            url: mailchimp_url,
+            type: "POST",
+            data: data,
+            dataType: 'json',
+            success: function(response, text) {
+            	if (snackbar) {
+            		snackbar.close();
+            	}
+                if (response.result === 'success') {
+                	msgParams['message'] = success_message;
+                    $('#email').val('');
+                } else {
+                	msgParams['message'] = response.result + ": " + response.msg;
+                	msgParams['timeout'] = 4000;
+                	$('#email').focus().select();
+                }
+                mdui.snackbar(msgParams);
+                $('.btn-email-subscibe').removeAttr('disabled');
+                mdui.updateTextFields('#email')
+            },
+            dataType: 'jsonp',
+            error: function(response, text) {
+                console.log('mailchimp ajax submit error: ' + text);
+                $('.btn-email-subscibe').removeAttr('disabled');
+                $('#email').focus().select();
+                mdui.updateTextFields('#email');
+            }
+        });
+        return false;
+    } else {
+    	msgParams['message'] = '请填写正确的邮箱地址';
+        mdui.snackbar(msgParams);
+        $('.btn-email-subscibe').removeAttr('disabled');
+        $('#email').focus().select();
+        mdui.updateTextFields('#email');
+    }
+};
+
+$('.btn-email-subscibe').click(attemptSubscibe);
